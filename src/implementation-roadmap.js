f57fcfe314052320d811b4d9deceaf0c4fade1f5
Ege Hidayet Koca <kocaege777@gmail.com>
2025-04-07 11:14:00 +0300

    feat: add core domain model

--- src/implementation-roadmap.js
+++ src/implementation-roadmap.js
@@ -3,3 +3,8 @@ export const stage1 = {
   "date": "2025-04-01",
   "message": "chore: initialize project structure"
 };
+
+export const stage2 = {
+  "date": "2025-04-07",
+  "message": "feat: add core domain model"
+};
